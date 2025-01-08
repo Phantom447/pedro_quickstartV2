d014645ba2104c06ca4f1638b9fc7ae58e7d3fd4
Johmm <76572867+Lightwastaken@users.noreply.github.com>
2025-01-07 17:36:14 -0800

    FIXED THINGS

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -7,23 +7,17 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 public class LConstants {
     static {
         TwoWheelConstants.forwardTicksToInches = .0021;
-        TwoWheelConstants.strafeTicksToInches = 0.0021;
-        TwoWheelConstants.forwardY = -5.625;
-        TwoWheelConstants.strafeX = -1.625;
-        // @TODO plug in odo(3 for foward odo and 0 for imu)>
-        TwoWheelConstants.forwardEncoder_HardwareMapName = "leftShift";
-        TwoWheelConstants.strafeEncoder_HardwareMapName = "foward";
-        // @TODO Check for reverse
-        TwoWheelConstants.forwardEncoderDirection = Encoder.FORWARD;
-        TwoWheelConstants.strafeEncoderDirection = Encoder.REVERSE;
+        TwoWheelConstants.strafeTicksToInches = .0021;
+        TwoWheelConstants.forwardY = 1;
+        TwoWheelConstants.strafeX = -2.5;
+        TwoWheelConstants.forwardEncoder_HardwareMapName = "leftFront";
+        TwoWheelConstants.strafeEncoder_HardwareMapName = "rightRear";
+        TwoWheelConstants.forwardEncoderDirection = Encoder.REVERSE;
+        TwoWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
         TwoWheelConstants.IMU_HardwareMapName = "imu";
-        // @TODO Check the orientation is right later
-        // @TODO Put IMU as port zero on control hub(refer to doc)
-        TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
-
+        TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
     }
 }
-
 
 
 
